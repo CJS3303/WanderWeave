d1e74559fdd99bec80e1c5b3037053cd3c13219d
CJS3303 <y335yuan@uwaterloo.ca>
2025-06-23 11:45:37 -0400

    Edited Layout

--- obj/Debug/net8.0/Project1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e65e6d9728d5fa0cbb82503126490b4569bfc3d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
